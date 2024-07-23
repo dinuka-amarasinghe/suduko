6553c870fc926bb6ac49270c1c2c28e4842a2290
dinukah99 <dinukahamarasinghe@gmail.com>
2024-07-23 22:28:15 +0530

    add IUserInterfaceContract for user interface event handling and view updates

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -3,6 +3,6 @@ package sudoku;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        SudokuApplication.main(new String[] {});
     }
 }
