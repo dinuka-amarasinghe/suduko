597f958cca760e19fbd79ba659d760b15bd91b4d
dinukah99 <dinukahamarasinghe@gmail.com>
2024-07-23 22:59:53 +0530

    draw sudoku board using rectangle

--- src/sudoku/userinterface/UserInterfaceImpl.java
+++ src/sudoku/userinterface/UserInterfaceImpl.java
@@ -122,9 +122,17 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View,
     }
 
     private void drawSudokuBoard(Group root) {
+        Rectangle boardBackground = new Rectangle();
+        boardBackground.setX(BOARD_PADDING);
+        boardBackground.setY(BOARD_PADDING);
+        boardBackground.setWidth(BOARD_X_AND_Y);
+        boardBackground.setHeight(BOARD_X_AND_Y);
+        boardBackground.setFill(BOARD_BACKGROUND_COLOR);
+        root.getChildren().addAll(boardBackground);
     }
 
     private void drawTitle(Group root) {
+
     }
 
     private void drawBackground(Group root) {
